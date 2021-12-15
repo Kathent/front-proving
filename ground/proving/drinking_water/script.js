009b4d6762a9d9ad28e7c1afc7db8fa54ffec511
liangkj <liangkaijun52013@gmail.com>
2021-12-15 09:59:19 +0800

    drinking water

--- ground/proving/drinking_water/script.js
+++ ground/proving/drinking_water/script.js
@@ -1,10 +1,17 @@
 let smallCup = document.getElementsByClassName("small-cup");
 let remainedDiv = document.getElementsByClassName("remained");
+let consumedDiv = document.getElementById("consumed");
 for (let x = 0; x < smallCup.length; x++) {
   let tmp = x;
   smallCup.item(tmp).addEventListener("click", () => {
     console.log("tmp is: ", tmp);
-    let remained = 2 - (2 * (tmp + 1.0)) / 8;
+    let consumed = (2 * (tmp + 1.0)) / 8;
+    let remained = 2 - consumed;
     remainedDiv.item(0).children.item(0).textContent = remained + "L";
+    consumedDiv.textContent = ((tmp + 1.0) / 8) * 100 + "%";
+    let height = `${(consumed / 2) * 330}px`;
+    consumedDiv.style.height = height;
+    consumedDiv.style.backgroundColor = "#6ab3f8";
+    console.log("consumedDiv: ", consumedDiv, consumedDiv.style.height, height);
   });
 }
